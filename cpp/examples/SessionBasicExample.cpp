17f2d47cead1e6873da4f1b3b5a22fdbcd46055f
shylock <33566796+Shylock-Hg@users.noreply.github.com>
2022-07-20 14:23:12 +0800

    Add simple test and example. (#5)

---
+++ cpp/examples/SessionBasicExample.cpp
@@ -0,0 +1,21 @@
+// Copyright (c) 2022 vesoft inc. All rights reserved.
+
+#include <common/Init.h>
+#include <nebula/client/Config.h>
+#include <nebula/client/ConnectionPool.h>
+
+#include <atomic>
+#include <chrono>
+#include <thread>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    nebula::client::init(&argc, &argv);
+    nebula::client::ConnectionPool pool;
+    pool.init({"127.0.0.1:22347"}, nebula::client::Config{});
+    auto session = pool.getSession(nebula::client::AuthReq{"root", "nebula", "graph", "v5.0.0"});
+    assert(session.valid());
+    auto resp = session.execute("malformed query.");
+    std::cout << "result: " << resp.executionOutcome_.gqlStatus_.status_ << std::endl;
+    return 0;
+}
