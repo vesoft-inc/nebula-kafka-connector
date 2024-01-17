3a38e21dc989f0be0da0ace73651ca40244ec20c
codesigner <codesignero@gmail.com>
2024-01-17 19:50:10 +0800

    [grpc] add grpc connection & protobuf integration (#290)
    * [grpc] add grpc connection & protobuf integration
    
    * fix format
    
    * fix format
    
    * sync graph.proto
    
    ---------
    
    Co-authored-by: jimingquan <6930445+nevermore3@users.noreply.github.com>

---
+++ java/client/src/main/java/com/vesoft/nebula/client/graph/net/GrpcConnection.java
@@ -0,0 +1,107 @@
+package com.vesoft.nebula.client.graph.net;
+
+import com.google.common.base.Charsets;
+import com.google.protobuf.ByteString;
+import com.vesoft.nebula.client.graph.data.HostAddress;
+import com.vesoft.nebula.client.graph.exception.AuthFailedException;
+import com.vesoft.nebula.client.graph.exception.ClientServerIncompatibleException;
+import com.vesoft.nebula.client.graph.exception.IOErrorException;
+import com.vesoft.nebula.proto.AuthRequest;
+import com.vesoft.nebula.proto.AuthResponse;
+import com.vesoft.nebula.proto.ExecuteRequest;
+import com.vesoft.nebula.proto.ExecuteResponse;
+import com.vesoft.nebula.proto.GraphGrpc;
+import com.vesoft.nebula.proto.SignoutRequest;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import java.nio.charset.Charset;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GrpcConnection extends Connection {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GrpcConnection.class);
+
+    private ManagedChannel channel;
+    private GraphGrpc.GraphBlockingStub stub;
+    private int connTimeout = 0;
+    private int requestTimeout = 0;
+
+    private Charset charset = Charsets.UTF_8;
+
+    @Override
+    public void open(HostAddress address, int connTimeout, int requestTimeout)
+            throws IOErrorException {
+        this.serverAddr = address;
+        this.connTimeout = connTimeout <= 0 ? Integer.MAX_VALUE : connTimeout;
+        this.requestTimeout = requestTimeout <= 0 ? Integer.MAX_VALUE : requestTimeout;
+        channel = ManagedChannelBuilder
+                .forAddress(address.getHost(), address.getPort()).usePlaintext()
+                .build();
+        stub = GraphGrpc.newBlockingStub(channel);
+    }
+
+    @Override
+    public void reopen() throws IOErrorException, ClientServerIncompatibleException {
+        // TODO
+    }
+
+    @Override
+    public void close() {
+        channel.shutdownNow();
+    }
+
+    @Override
+    public boolean ping(long sessionID) throws IOErrorException {
+        return false;
+    }
+
+    public AuthResult authenticate(String user, String password)
+            throws AuthFailedException, IOErrorException {
+        try {
+            AuthRequest authReq = AuthRequest.newBuilder()
+                    .setUsername(ByteString.copyFrom(user, charset))
+                    .setPassword(ByteString.copyFrom(password, charset))
+                    .setClientType(ByteString.copyFrom("java", charset))
+                    .setClientVersion(ByteString.copyFrom("v5.0.0", charset))
+                    .build();
+            AuthResponse resp = stub.authenticate(authReq);
+            if (!"SUCCESS".equals(new String(resp.getGqlStatus().getStatus().toString(charset)))) {
+                if (resp.getGqlStatus().getStatus() != null) {
+                    throw new AuthFailedException(resp.getGqlStatus().getStatus().toString());
+                } else {
+                    throw new AuthFailedException(
+                            "The error_msg is null, "
+                                    + "maybe the service not set or the response is disorder.");
+                }
+            }
+            return new AuthResult(resp.getSessionId());
+        } catch (Exception e) {
+            // TODO
+            throw e;
+        }
+    }
+
+    public ExecuteResponse execute(long sessionID, String stmt) throws IOErrorException {
+        try {
+            ExecuteRequest request = ExecuteRequest.newBuilder()
+                    .setSessionId(sessionID)
+                    .setStmt(ByteString.copyFrom(stmt, charset))
+                    .build();
+            return stub.execute(request);
+        } catch (Exception e) {
+            // TODO
+            throw e;
+        }
+    }
+
+    public void signout(long sessionID) {
+        try {
+            SignoutRequest request = SignoutRequest.newBuilder().setSessionId(sessionID).build();
+            stub.signout(request);
+        } catch (Exception e) {
+            // TODO
+            throw e;
+        }
+    }
+}
---
+++ java/client/src/test/java/com/vesoft/nebula/client/graph/net/TestGrpcConnection.java
@@ -0,0 +1,60 @@
+/* Copyright (c) 2022 vesoft inc. All rights reserved.
+ *
+ * This source code is licensed under Apache 2.0 License.
+ */
+
+package com.vesoft.nebula.client.graph.net;
+
+import com.google.protobuf.ByteString;
+import com.vesoft.nebula.client.graph.data.HostAddress;
+import com.vesoft.nebula.proto.ExecuteResponse;
+import java.nio.charset.Charset;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestGrpcConnection {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GrpcConnection.class);
+
+    @Test(timeout = 3000)
+    public void testAll() {
+        try {
+            // Test open
+            GrpcConnection connection = new GrpcConnection();
+            connection.open(new HostAddress("127.0.0.1", 14304), 1000, 1000);
+
+            // Test authenticate
+            AuthResult authResult = connection.authenticate("root", "nebula");
+            LOGGER.info(authResult.toString());
+
+            Assert.assertNotEquals(0, authResult.getSessionId());
+
+            // Test execute
+            ExecuteResponse resp = connection.execute(authResult.getSessionId(), "SHOW GRAPHS");
+
+            LOGGER.info(resp.toString());
+
+            Assert.assertEquals(ByteString.copyFrom("SUCCESS",
+                    Charset.defaultCharset()),
+                    resp.getExecutionOutcome().getGqlStatus().getStatus());
+
+            resp = connection.execute(authResult.getSessionId(), "USE ldbc\n"
+                    + " MATCH (v:Person WHERE v.id IN LIST[2, 3, 4, 888])-[e:KNOWS]->{0,1}(b)\n"
+                    + " RETURN v.id as src, b.id as dst");
+
+            LOGGER.info(resp.toString());
+
+            Assert.assertEquals(ByteString.copyFrom("SUCCESS",
+                            Charset.defaultCharset()),
+                    resp.getExecutionOutcome().getGqlStatus().getStatus());
+
+            // Test sign out
+            connection.signout(authResult.getSessionId());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            assert (false);
+        }
+    }
+}
